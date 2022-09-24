320c1ebc2bfb2e8bc9a933fb24dc32d945468bc7
statho7 <andreas.stathop@gmail.com>
2022-09-24 09:38:08 +0100

    Test conditions with if/else expressions

--- src/main.go
+++ src/main.go
@@ -1,37 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+import "fmt"
 
+func somenumber() int {
+	return -7
+}
 func main() {
-	sum, mul := calc(os.Args[1], os.Args[2])
-	fmt.Println("Sum:", sum)
-	fmt.Println("Mul:", mul)
+	num := somenumber()
+	if num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
 
-	firstName := "John"
-	updateName(firstName)
-	fmt.Println(firstName)
-
-	firstName2 := "Mike"
-	updateNamePointer(&firstName2)
-	fmt.Println(firstName2)
-}
-
-func calc(number1 string, number2 string) (int, int) {
-	int1, _ := strconv.Atoi(number1)
-	int2, _ := strconv.Atoi(number2)
-	sum := int1 + int2
-	mul := int1 * int2
-	return sum, mul
-}
-
-func updateName(name string) {
-	name = "David"
-}
-
-func updateNamePointer(name *string) {
-	*name = "David"
+	fmt.Println(num)
 }
